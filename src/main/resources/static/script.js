838bec2c8b79eb1b759e1282f2518b596b633a1f
Bartłomiej Sadłoń <bartek.sadlon2000@gmail.com>
2023-08-27 18:20:01 +0200

    nowe

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -5,4 +5,8 @@ const myButton = document.getElementById('myButton');
 myButton.addEventListener('click', function() {
     // Kod do wykonania po kliknięciu przycisku
     alert('Przycisk został kliknięty!');
+    fetch("http://localhost:8080/")
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Wystąpił błąd:', error));
 });
